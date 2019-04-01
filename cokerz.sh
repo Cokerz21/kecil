d665e8b318863075fdd2c53c022a9be3dc5c90b5
Cokerz21 <48947784+Cokerz21@users.noreply.github.com>
2019-04-01 17:32:47 +0700

    Add files via upload

---
+++ cokerz.sh
@@ -0,0 +1,170 @@
+clear
+bi='\033[34;1m' #BIRU
+i='\033[32;1m' #IJO
+pur='\033[35;1m' #PURPLE
+cy='\033[36;1m' #CYAN
+me='\033[31;1m' #MERAH
+pu='\033[37;1m' #PUTIH
+ku='\033[33;1m' #KUNING
+echo
+echo
+python2 meizu.py
+echo
+clear
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu Lae :)"   
+sleep 1
+echo '''\a
+\033[34;1m               _
+\033[34;1m              / |
+\033[34;1m              | |
+\033[34;1m              | |
+\033[34;1m             _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu Lae :)"
+sleep 1
+echo '''\a
+\033[32;1m              ____
+\033[32;1m             |___ \
+\033[32;1m               __) |
+\033[32;1m              / __/
+\033[32;1m             |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu Lae :)"
+sleep 1
+echo '''\a
+\033[35;1m             _____
+\033[35;1m            |___ /
+\033[35;1m              |_ \
+\033[35;1m             ___) |
+\033[35;1m            |____/ '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu Lae :)"
+sleep 1
+echo '''\a
+\033[33;1m             _  _
+\033[33;1m            | || |
+\033[33;1m            | || |_
+\033[33;1m            |__   _|
+\033[33;1m               |_| '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu Lae :)"
+sleep 1
+echo '''\a
+\033[36;1m              ____
+\033[36;1m             | ___|
+\033[36;1m             |___ \
+\033[36;1m              ___) |
+\033[36;1m             |____/ '''
+sleep 1
+clear
+echo $pur"
+    _   ____ __  _   ______  ____            
+   / | / / // / / | / / __ \/ __ \  __  __/|_
+  /  |/ / // /_/  |/ / / / / / / /_/ /_|    /
+ / /|  /__  __/ /|  / /_/ / /_/ /_  __/_ __| 
+/_/ |_/  /_/ /_/ |_/_____/\____/ /_/   |/    "
+echo $me"─────────────────────────────────────────────────"
+echo $cy"█"$cy"  Author  :"$cy"[ N4ND0 ]     "$cy"                 █"
+echo $me"█"$pu"  WhatsApp:"$cy"[ 083800935xxx ] "$me"              █"
+echo $cy"█"$pu"  Gmail   :"$cy"[ jekikucinglucu21@gmail.com] "$cy" █"
+echo $me"█"$pu"  TEAM    :"$cy"[ MessTe4m ] "$me"                  █"
+echo $me"─────────────────────────────────────────────────"
+echo
+echo $cy "───────────"$cy "MENU HACK BROZz21" $cy"───────────"
+echo $cy"║"                                               $cy"║"
+echo $ku"============================================="
+echo $ku"║"$cy"║▶"$ku"1"$cy"◀║"$ku" MBF"          $ku"║"                
+echo $ku"============================================="
+echo $ku"║"$cy"║▶"$ku"2"$cy"◀║"$ku" Cloning Yahoo"$ku"║"        
+echo $ku"============================================="
+echo $ku"║"$cy"║▶"$ku"3"$cy"◀║"$me" EXIT"         $ku"║"                                                                                                                     
+echo $ku"============================================="
+echo $cy"║"                                               $cy"║"
+echo $cy "───────────"$cy "MENU HACK BROZz21 "$cy"───────────" 
+echo 
+echo $pu"╭─["$i"PILIH SESUAI KEINGINAN ANDA"$pu"]"
+read -p"╰─> : " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "N4ND0"|lolcat
+sleep 1
+git clone https://github.com/brozz21/mbf.git
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "N4ND0"|lolcat
+sleep 1
+git clone https://github.com/brozz21/YahooClone.git
+cd YahooClone
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "E X I T"|Ulolcat
+sleep 2
+echo $i"SEMANGAT PAGI..."
+echo $i"Terima Kasih Lae sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan  Lae di pakai semoga bermanfaat"
+sleep 2
+echo $i"Buat Lae²"
+sleep 2
+echo $i"Bila Lae belum mengerti Lae Bisa Nanya"
+echo $i"Melalui Via:"
+sleep 2
+echo $ku"Facebook :"$pur" Brozz21"
+echo $ku"YouTube  :"$pur" Brozz21"
+echo $ku"WhatsApp :"$pur" 083800935xxx"
+sleep 2
+echo $i"Brozz21 "$cy" LIKE SHARE AND SUBSCRIBE"
+sleep 2
+echo $i"Video Channel "$cy" Brozz21"
+sleep 2
+echo $i"Agar tambah semangat Upload Video²"
+sleep 2
+echo $i"terbaru menarik dari saya"
+echo $i"no system is safe"
+sleep 2
+echo $i"Agar dapat pemberitahuan Video²"
+echo $i"selanjutnya dari saya OKE"
+sleep 2
+echo
+echo $cy"COKERZ21"
+exit
+fi
